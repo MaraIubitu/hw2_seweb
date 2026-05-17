b007a73225352b883b627be095aa83a44c9a849a
Gabriel-x-1 <gabi2015a1b1@gmail.com>
2026-05-18 02:51:38 +0300

    Add gitignore and remove unnecessary files

--- hw2/extract_pdf2.py
+++
@@ -1,6 +0,0 @@
-import pdfplumber
-
-pdf_path = r"c:\Users\Mara\Desktop\seweb\hw2\Semantic Web Homework 2.pdf"
-with pdfplumber.open(pdf_path) as pdf:
-    for page in pdf.pages:
-        print(page.extract_text())
